3c825bdb3309d0e2d3b749ca5cd4cecf890e312c
OMARTK1 <elyazidi.omar@gmail.com>
2024-02-24 20:25:50 +0100

    flask task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,7 @@
-# Using Puppet, install flask from pip3
+# Puppet manifest to install Flask version 2.1.0
+
+# Ensure the package is installed
 package { 'flask':
-  ensure   => '2.1.0',
+  ensure => '2.1.0',
   provider => 'pip3',
 }
