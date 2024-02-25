b87c718d84628d9882d7f5bbc03fb5926e07231f
OMARTK1 <elyazidi.omar@gmail.com>
2024-02-26 00:30:16 +0100

    werkzeug task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,7 @@
 #!/usr/bin/puppet
 
 # Install a specific version of flask (2.1.0)
-package { 'flask':
-  ensure   => '2.1.0',
+package { 'Werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
-
