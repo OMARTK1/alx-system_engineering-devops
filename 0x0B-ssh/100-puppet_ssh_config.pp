378543be5eba0aa88bd898122855ef2dd986ac94
OMARTK1 <elyazidi.omar@gmail.com>
2024-02-26 03:11:14 +0100

    100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,13 @@
-file_line { 'Turn off passwd auth':
-  path => '/etc/ssh/ssh_config',
-  line => '    PasswordAuthentication no',
+# Include the stdlib module
+include stdlib
+
+# Configure SSH client to use the private key and disable password authentication
+file_line { 'Turn off password authentication':
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
 }
 
 file_line { 'Declare identity file':
-  path => '/etc/ssh/ssh_config',
-  line => '    IdentityFile ~/.ssh/school',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
 }
